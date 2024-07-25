d25d288c42933b4b8a13b595bfcc4b68add4516c
Maxim-Chan <vi0leten77@gmail.com>
2024-07-24 20:47:11 -0400

    Remove body parser library and use express  built-in body parser

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const db = new pg.Client({
