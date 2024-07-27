1f644f0e8f3b8de768b5655d4ff3d8a5e6af132f
Maxim-Chan <vi0leten77@gmail.com>
2024-07-27 00:46:11 -0400

    New feature allowing to add visited countries to the database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { count } from "console";
 import express from "express";
 import pg from "pg";
 
@@ -22,8 +23,25 @@ app.get("/", async (req, res) => {
   const visited_countries = result.rows.map(country => country.country_code);
 
   res.render("index.ejs", {countries: visited_countries, total: visited_countries.length})
-  db.end()
+
 });
+
+app.post("/add", async (req, res) => {
+
+  const inputCountry = req.body.country;
+  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [inputCountry]);
+  
+  if (result.rows.length > 0){
+    const country_code = result.rows[0].country_code;
+    
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [country_code]);
+    res.redirect("/");
+  } else {
+    console.error("Error 404. This country does not exist");
+    res.redirect("/");
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
